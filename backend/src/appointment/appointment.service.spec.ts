4cb83c8d2c45e2bc4e2b8b5d9e4fc54ed67c5e19
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-28 19:12:59 -0400

    update: modify imports

--- backend/src/appointment/appointment.service.spec.ts
+++ backend/src/appointment/appointment.service.spec.ts
@@ -3,10 +3,9 @@ import { AppointmentService } from './appointment.service'
 import { validate } from 'class-validator'
 import { CreateAppointmentDto } from './dto/create-appointment.dto'
 import { plainToInstance } from 'class-transformer'
-import { appointmentMock } from '../../test/mocks/appointment.mock'
+import { appointmentMock, mockPrisma } from '../../test/mocks/'
 import { AppointmentController } from './appointment.controller'
 import { PrismaService } from '../prisma/prisma.service'
-import { prismaMock } from '../../test/mocks/prisma.mock'
 import { AuthService } from '../auth/auth.service'
 
 const createAppointmentDto = plainToInstance(CreateAppointmentDto, appointmentMock)
@@ -22,13 +21,13 @@ describe('AppointmentService', () => {
         AuthService,
         {
           provide: PrismaService,
-          useValue: prismaMock
+          useValue: mockPrisma
         }
       ]
     }).compile()
 
     appointmentService = module.get<AppointmentService>(AppointmentService)
-    prismaMock.appointment.create.mockClear()
+    mockPrisma.appointment.create.mockClear()
   })
 
   describe('Create appointment', () => {
@@ -37,26 +36,26 @@ describe('AppointmentService', () => {
       expect(error.length).toBe(0)
     })
     it('should return a new appointment', async () => {
-      prismaMock.employeeCompany.findUnique.mockResolvedValue({
+      mockPrisma.employeeCompany.findUnique.mockResolvedValue({
         id: 'randomUUID'
       })
-      prismaMock.user.findUnique.mockResolvedValue({
+      mockPrisma.user.findUnique.mockResolvedValue({
         id: 'randomUUID'
       })
-      prismaMock.service.findUnique.mockResolvedValue({
+      mockPrisma.service.findUnique.mockResolvedValue({
         id: 'randomUUID'
       })
-      prismaMock.serviceAppointment.createMany.mockResolvedValue({
+      mockPrisma.serviceAppointment.createMany.mockResolvedValue({
         id: 'randomUUID'
       })
-      prismaMock.appointment.create.mockResolvedValue({
+      mockPrisma.appointment.create.mockResolvedValue({
         ...appointmentMock,
         id: 'randomUUID'
       })
 
       await appointmentService.create(createAppointmentDto)
 
-      expect(prismaMock.employeeCompany.findUnique).toHaveBeenCalledTimes(1)
+      expect(mockPrisma.employeeCompany.findUnique).toHaveBeenCalledTimes(1)
     })
   })
 })
