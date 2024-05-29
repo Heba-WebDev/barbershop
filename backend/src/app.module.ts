d07ad33658fc1e567918006d0ca9de1cc156065f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-29 16:13:24 -0400

    update: Added in imports ServiceModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config';
+import { ServiceModule } from './service/service.module';
 
 
 @Module({
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ServiceModule,
   ]
 })
 export class AppModule {}
