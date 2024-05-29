4e6bee1b1de46ac85bbab432ea4e5a313e5d4aa8
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-29 16:36:35 -0400

    chore: added valiations;

--- backend/src/service/dto/create-service.dto.ts
+++ backend/src/service/dto/create-service.dto.ts
@@ -1,1 +1,13 @@
-export class CreateServiceDto {}
+import { IsNumber, IsString, IsUUID } from 'class-validator'
+import { UUID } from 'crypto'
+
+export class CreateServiceDto {
+  @IsString()
+    name: string
+
+  @IsNumber()
+    value: number
+
+  @IsUUID()
+    user_id: UUID
+}
