7d58d07a2dd57e2c0c16127777e661e49b8cd99f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-29 19:22:42 -0400

    feat: add imports PrismaModule

--- backend/src/service/service.module.ts
+++ backend/src/service/service.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ServiceService } from './service.service';
-import { ServiceController } from './service.controller';
+import { Module } from '@nestjs/common'
+import { ServiceService } from './service.service'
+import { ServiceController } from './service.controller'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   controllers: [ServiceController],
   providers: [ServiceService],
+  imports: [PrismaModule]
 })
 export class ServiceModule {}
