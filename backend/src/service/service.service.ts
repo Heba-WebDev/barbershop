8637832ff5da61c10351d4ddd546c16ba3344e2f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-29 17:00:33 -0400

    chore: add something

--- backend/src/service/service.service.ts
+++ backend/src/service/service.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { type CreateServiceDto } from './dto/create-service.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { type UUID } from 'crypto'
 
 @Injectable()
 export class ServiceService {
+  constructor (
+    private readonly prisma: PrismaService
+  ) {}
+
   create (createServiceDto: CreateServiceDto) {
+    // const service = this.prisma.service.create({
+    //   data: {}
+    // })
     return 'This action adds a new service'
+  }
+
+  async findServiceUUID (id: UUID) {
+    try {
+      return await this.prisma.service.findUnique({
+        where: { id }
+      })
+    } catch (error) {
+      throw new InternalServerErrorException('Error in Find by Service UUID')
+    }
   }
 }
