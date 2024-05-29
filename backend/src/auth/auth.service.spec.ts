ee28bdc9887b8069c3f283cc237370f10eec70b2
vitalspace <myglobalsounds@gmail.com>
2024-05-28 20:18:09 -0600

    Update auth.service.spec.ts

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,132 +1,84 @@
-import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
-  BadRequestException,
-  InternalServerErrorException
-} from "@nestjs/common";
+import { ConflictException } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { Test, type TestingModule } from '@nestjs/testing'
+import { validate } from 'class-validator'
 
-import * as jwt from "jsonwebtoken";
-import { hash, compare } from "bcrypt";
+import { hash } from 'bcrypt'
 
-import { type CreateUserDto } from "./dto/create-user.dto";
-import { type LoginUserDto } from "./dto/login-user.dto";
-import { PrismaService } from "../prisma/prisma.service";
-import { type User, type IJwtPayload } from "./interfaces";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { type UUID } from 'crypto'
+import { type User } from './interfaces'
+import { PrismaService } from '../prisma/prisma.service'
+import { AuthService } from './auth.service'
 
+import { AuthController } from './auth.controller'
+import { mockPrisma, mockUser } from '../../test/mocks'
 
-@Injectable()
-export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+jest.mock('bcrypt', () => ({
+  hash: jest.fn().mockResolvedValue('hashedPassword')
+}))
 
-  async create(createAuthDto: CreateUserDto) {
-    const { email, password, name, phoneNumber } = createAuthDto;
+describe('AuthService', () => {
+  let authService: AuthService
 
-    const user = await this.findUserByEmail(email);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: mockPrisma
+        }
+      ],
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })]
+    })
+      .compile()
 
-    if (user)
-      throw new ConflictException(
-        `User with email ${email} is already registered`
-      );
-    const hashedPass = await hash(password, 10);
-    const { id, role } = await this.prismaService.user.create({
-      data: {
-        name,
-        email,
-        phone_number: createAuthDto.phoneNumber,
-        password: hashedPass,
-        role: "CLIENT",
-      },
-    });
+    authService = module.get<AuthService>(AuthService)
+    mockPrisma.user.create.mockClear()
+  })
 
-    return { user: { id, name, email, phoneNumber, role } };
-  }
+  describe('create user', () => {
+    beforeEach(async () => {
+      const errors = await validate(mockUser)
+      expect(errors.length).toBe(0)
+    })
 
-  async findUserByEmail(email: string) {
-    const user = await this.prismaService.user.findUnique({ where: { email } });
-    return user;
-  }
+    it('should return a new user', async () => {
+      const { user } = await authService.create(mockUser)
 
-  async login(loginAuthDto: LoginUserDto) {
-    const { email, password } = loginAuthDto;
-
-    const user = await this.findUserByEmail(email);
-
-    if (!user) throw new NotFoundException("User not found");
-
-    const pass = await compare(password, user.password);
-
-    if (!pass) throw new BadRequestException("Invalid credentials");
-
-    const token = await this.generateJwt({ id: user.id });
-
-    return {
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        phone_number: user.phone_number,
-        is_active: user.is_active,
-        is_verified: user.is_verified,
-        avatar: user.avatar,
-        role: user.role,
-      },
-      token,
-    };
-  }
-
-  async update(
-    id: string,
-    data: UpdateUserDto
-  ): Promise<{
-    name: string;
-    email: string;
-    phone_number: string;
-    avatar: string;
-  }> {
-    const user = await this.prismaService.user.findUnique({ where: { id } });
-
-    if (!user) throw new NotFoundException("User doesn't exist");
-
-    return this.prismaService.user.update({
-      where: {
-        id,
-      },
-      data,
-      select: {
-        name: true,
-        email: true,
-        phone_number: true,
-        avatar: true,
-      },
-    });
-  }
-
-  async generateJwt(payload: IJwtPayload) {
-    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_REFRESH_EXPIRATION,
-    });
-    return token;
-  }
-
-  async renewToken(user: User) {
-    const token = await this.generateJwt({ id: user.id });
-
-    return {
-      user,
-      token,
-    };
-  }
-
-   async findUserByUUID (id: UUID) {
-    try {
-      return await this.prismaService.user.findUnique({
-        where: { id }
+      expect(mockPrisma.user.create).toHaveBeenCalledTimes(1)
+      expect(user).toMatchObject({
+        id: expect.any(String),
+        name: mockUser.name,
+        email: mockUser.email,
+        phoneNumber: mockUser.phoneNumber,
+        role: 'CLIENT'
       })
-    } catch (error) {
-      throw new InternalServerErrorException('Error in Find by User UUID')
-    }
-  }
-}
+
+      expect(user).not.toHaveProperty('password')
+    })
+
+    it('should return a registered user conflict error exception', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(mockUser)
+
+      await expect(authService.create(mockUser)).rejects.toThrow(new ConflictException(`User with email ${mockUser.email} is already registered`))
+    })
+  })
+
+  describe('renew token', () => {
+    it('shoult return an user with new token', async () => {
+      const newMockUser: User = {
+        id: 'randomUUID',
+        email: mockUser.email,
+        name: mockUser.name,
+        phoneNumber: mockUser.phoneNumber,
+        role: 'CLIENT'
+      }
+
+      const { token, user } = await authService.renewToken(newMockUser)
+
+      expect(user).toEqual(newMockUser)
+      expect(typeof token).toBe('string')
+    })
+  })
+})
