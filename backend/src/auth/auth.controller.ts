3a3dbe60eccab65d821c81c4e98ab1bcd4b9e437
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-30 00:53:43 +0300

    feat: added forgot-password and reset-password

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,34 +1,49 @@
-import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post, Put, Req, UseGuards } from '@nestjs/common'
 
-import { Auth, GetUser } from "./decorators";
-import { AuthService } from "./auth.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { LoginUserDto } from "./dto/login-user.dto";
-import { User } from "./interfaces";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { Auth, GetUser } from './decorators'
+import { AuthService } from './auth.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { LoginUserDto } from './dto/login-user.dto'
+import { User } from './interfaces'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { ForgotPasswordUserDto } from './dto/forgot-password.dto'
+import { ResetPassUserDto } from './dto/reset-password.dto'
+import { Request } from 'express'
+import { JWTAuthGuard } from './guards/reset-pass.guard'
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor (private readonly authService: AuthService) {}
 
-  @Post("new-account")
-  async create(@Body() createAuthDto: CreateUserDto) {
-    return await this.authService.create(createAuthDto);
+  @Post('new-account')
+  async create (@Body() createAuthDto: CreateUserDto) {
+    return await this.authService.create(createAuthDto)
   }
 
-  @Post("login")
-  async login(@Body() loginAuthDto: LoginUserDto) {
-    return await this.authService.login(loginAuthDto);
+  @Post('login')
+  async login (@Body() loginAuthDto: LoginUserDto) {
+    return await this.authService.login(loginAuthDto)
   }
 
-  @Get("renew")
+  @Get('renew')
   @Auth()
-  async renewToken(@GetUser() user: User) {
-    return await this.authService.renewToken(user);
+  async renewToken (@GetUser() user: User) {
+    return await this.authService.renewToken(user)
   }
 
-  @Put(":id")
-  async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
-    return await this.authService.update(id, updateUserDto);
+  @Put(':id')
+  async update (@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.authService.update(id, updateUserDto)
+  }
+
+  @Post('forgot-password')
+  async forgotPassword (@Body() email: ForgotPasswordUserDto) {
+    return await this.authService.forgotPassword(email)
+  }
+
+  @Patch('reset-password')
+  @UseGuards(JWTAuthGuard)
+  async resetPassword (@Req() req: Request, @Body() resetPass: ResetPassUserDto) {
+    return await this.authService.resetPassword(resetPass)
   }
 }
