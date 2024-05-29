792a471d3733e3fbe390bcfc50282e9aeb91d955
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-30 00:55:57 +0300

    feat: added forgot password, rest password and send email

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,64 +3,68 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
-  InternalServerErrorException
-} from "@nestjs/common";
+  InternalServerErrorException,
+  ServiceUnavailableException
+} from '@nestjs/common'
 
-import * as jwt from "jsonwebtoken";
-import { hash, compare } from "bcrypt";
+import * as jwt from 'jsonwebtoken'
+import { hash, compare } from 'bcrypt'
+import { createTransport } from 'nodemailer'
 
-import { type CreateUserDto } from "./dto/create-user.dto";
-import { type LoginUserDto } from "./dto/login-user.dto";
-import { PrismaService } from "../prisma/prisma.service";
-import { type User, type IJwtPayload } from "./interfaces";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { type CreateUserDto } from './dto/create-user.dto'
+import { type LoginUserDto } from './dto/login-user.dto'
+import { PrismaService } from '../prisma/prisma.service'
+import { type User, type IJwtPayload } from './interfaces'
+import { type UpdateUserDto } from './dto/update-user.dto'
 import { type UUID } from 'crypto'
-
+import { type ForgotPasswordUserDto } from './dto/forgot-password.dto'
+import { type ResetPassUserDto } from './dto/reset-password.dto'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor (private readonly prismaService: PrismaService) {}
 
-  async create(createAuthDto: CreateUserDto) {
-    const { email, password, name, phoneNumber } = createAuthDto;
+  async create (createAuthDto: CreateUserDto) {
+    const { email, password, name, phoneNumber } = createAuthDto
 
-    const user = await this.findUserByEmail(email);
+    const user = await this.findUserByEmail(email)
 
-    if (user)
+    if (user) {
       throw new ConflictException(
         `User with email ${email} is already registered`
-      );
-    const hashedPass = await hash(password, 10);
+      )
+    }
+    const hashedPass = await hash(password, 10)
     const { id, role } = await this.prismaService.user.create({
       data: {
         name,
         email,
         phone_number: createAuthDto.phoneNumber,
         password: hashedPass,
-        role: "CLIENT",
-      },
-    });
+        role: 'CLIENT'
+      }
+    })
 
-    return { user: { id, name, email, phoneNumber, role } };
+    return { user: { id, name, email, phoneNumber, role } }
   }
 
-  async findUserByEmail(email: string) {
-    const user = await this.prismaService.user.findUnique({ where: { email } });
-    return user;
+  async findUserByEmail (email: string) {
+    const user = await this.prismaService.user.findUnique({ where: { email } })
+    return user
   }
 
-  async login(loginAuthDto: LoginUserDto) {
-    const { email, password } = loginAuthDto;
+  async login (loginAuthDto: LoginUserDto) {
+    const { email, password } = loginAuthDto
 
-    const user = await this.findUserByEmail(email);
+    const user = await this.findUserByEmail(email)
 
-    if (!user) throw new NotFoundException("User not found");
+    if (!user) throw new NotFoundException('User not found')
 
-    const pass = await compare(password, user.password);
+    const pass = await compare(password, user.password)
 
-    if (!pass) throw new BadRequestException("Invalid credentials");
+    if (!pass) throw new BadRequestException('Invalid credentials')
 
-    const token = await this.generateJwt({ id: user.id });
+    const token = await this.generateJwt({ id: user.id })
 
     return {
       user: {
@@ -71,56 +75,112 @@ export class AuthService {
         is_active: user.is_active,
         is_verified: user.is_verified,
         avatar: user.avatar,
-        role: user.role,
+        role: user.role
       },
-      token,
-    };
+      token
+    }
   }
 
-  async update(
+  async update (
     id: string,
     data: UpdateUserDto
   ): Promise<{
-    name: string;
-    email: string;
-    phone_number: string;
-    avatar: string;
-  }> {
-    const user = await this.prismaService.user.findUnique({ where: { id } });
+      name: string
+      email: string
+      phone_number: string
+      avatar: string
+    }> {
+    const user = await this.prismaService.user.findUnique({ where: { id } })
 
-    if (!user) throw new NotFoundException("User doesn't exist");
+    if (!user) throw new NotFoundException("User doesn't exist")
 
-    return this.prismaService.user.update({
+    return await this.prismaService.user.update({
       where: {
-        id,
+        id
       },
       data,
       select: {
         name: true,
         email: true,
         phone_number: true,
-        avatar: true,
+        avatar: true
+      }
+    })
+  }
+
+  async forgotPassword (email: ForgotPasswordUserDto) {
+    const user = await this.findUserByEmail(email.email)
+
+    if (!user) throw new NotFoundException('User not found')
+    await this.sendEmail(user.id, user.email)
+    return 'Email succssfully sent'
+  }
+
+  async resetPassword (resetPassAuthDto: ResetPassUserDto) {
+    const usr = await this.findUserByEmail(resetPassAuthDto.email)
+
+    if (!usr) throw new NotFoundException('User not found')
+
+    const hashedPass = await hash(resetPassAuthDto.password, 10)
+    await this.prismaService.user.update({
+      where: {
+        id: usr.id
       },
-    });
+      data: {
+        password: hashedPass
+      }
+    })
+    const { id, password, ...user } = usr
+    const token = await this.generateJwt({ id })
+    return {
+      user,
+      token
+    }
   }
 
-  async generateJwt(payload: IJwtPayload) {
+  async generateJwt (payload: IJwtPayload) {
     const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_REFRESH_EXPIRATION,
-    });
-    return token;
+      expiresIn: process.env.JWT_REFRESH_EXPIRATION
+    })
+    return token
   }
 
-  async renewToken(user: User) {
-    const token = await this.generateJwt({ id: user.id });
+  async renewToken (user: User) {
+    const token = await this.generateJwt({ id: user.id })
 
     return {
       user,
-      token,
-    };
+      token
+    }
   }
 
-   async findUserByUUID (id: UUID) {
+  async sendEmail (id: string, email: string) {
+    const token = await this.generateJwt({ id })
+    const transporter = createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    })
+    const mailOptions = {
+      from: process.env.EMAIL_USR,
+      to: email,
+      subject: 'Reiniciar contraseña | Barbería',
+      text: `Gracias por usar nuestros servicos. Hemos recibido una solicitud para restablecer la contraseña de tu cuenta. Haz clic a este enlace ${process.env.BASE_URL}/reset-password/${token}. Si no solicitaste este cambio, ignora este correo electrónico.`
+    }
+    if (process.env.NODE_ENV !== 'test') {
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          throw new ServiceUnavailableException('An error occured while sending the email')
+        }
+      })
+    }
+  }
+
+  async findUserByUUID (id: UUID) {
     try {
       return await this.prismaService.user.findUnique({
         where: { id }
