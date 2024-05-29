7b4f2bcdd192a98aedabbe4b4121b8919c18c5b4
vitalspace <myglobalsounds@gmail.com>
2024-05-28 19:08:28 -0600

    feat: added user update controller + tests

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,28 +1,34 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 
-import { Auth, GetUser } from './decorators'
-import { AuthService } from './auth.service'
-import { CreateUserDto } from './dto/create-user.dto'
-import { LoginUserDto } from './dto/login-user.dto'
-import { User } from './interfaces'
+import { Auth, GetUser } from "./decorators";
+import { AuthService } from "./auth.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
+import { User } from "./interfaces";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor (private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Post('new-account')
-  async create (@Body() createAuthDto: CreateUserDto) {
-    return await this.authService.create(createAuthDto)
+  @Post("new-account")
+  async create(@Body() createAuthDto: CreateUserDto) {
+    return await this.authService.create(createAuthDto);
   }
 
-  @Post('login')
-  async login (@Body() loginAuthDto: LoginUserDto) {
-    return await this.authService.login(loginAuthDto)
+  @Post("login")
+  async login(@Body() loginAuthDto: LoginUserDto) {
+    return await this.authService.login(loginAuthDto);
   }
 
-  @Get('renew')
+  @Get("renew")
   @Auth()
-  async renewToken (@GetUser() user: User) {
-    return await this.authService.renewToken(user)
+  async renewToken(@GetUser() user: User) {
+    return await this.authService.renewToken(user);
+  }
+
+  @Put(":id")
+  async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.authService.update(id, updateUserDto);
   }
 }
