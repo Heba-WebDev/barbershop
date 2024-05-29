74f2f19c3529f48e81b8f94a8f1a72daf6376ebd
vitalspace <myglobalsounds@gmail.com>
2024-05-28 19:07:44 -0600

    feat: we can now read the environment variables globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
-    AuthModule
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ]
 })
 export class AppModule {}
