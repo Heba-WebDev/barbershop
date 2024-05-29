f534ba2d519980e328ced275b733f4927a8c4009
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-30 02:19:44 +0300

    feat: reset password dto

---
+++ backend/src/auth/dto/reset-password.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsStrongPassword } from 'class-validator'
+
+export class ResetPassUserDto {
+  @IsEmail()
+  readonly email: string
+
+  @IsStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minNumbers: 1,
+    minLowercase: 0,
+    minSymbols: 0
+  }, { message: 'The password must have at least 8 characters, 1 uppercase letter and 1 number' })
+  readonly password: string
+}
