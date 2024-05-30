190105c3b368e2516aca3f8899caa0c384b2acd6
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-30 19:02:37 -0400

    feat: new import authmodule

--- backend/src/service/service.module.ts
+++ backend/src/service/service.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { ServiceService } from './service.service'
 import { ServiceController } from './service.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
   controllers: [ServiceController],
   providers: [ServiceService],
-  imports: [PrismaModule]
+  imports: [PrismaModule, AuthModule]
 })
 export class ServiceModule {}
