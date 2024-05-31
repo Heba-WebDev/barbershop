d696121ad73ce2e5302b9bfe2fa53feb9d882a23
vitalspace <myglobalsounds@gmail.com>
2024-05-31 00:41:33 -0600

    Update auth.service.spec.ts

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,23 +1,26 @@
-import { ConflictException, Controller, NotFoundException } from "@nestjs/common";
-import { PassportModule } from "@nestjs/passport";
-import { Test, type TestingModule } from "@nestjs/testing";
-import { validate } from "class-validator";
+import {
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { Test, type TestingModule } from '@nestjs/testing'
+import { validate } from 'class-validator'
 
-import { hash } from "bcrypt";
+import { PrismaService } from '../prisma/prisma.service'
+import { AuthService } from './auth.service'
+import { type User } from './interfaces'
 
-import { type User } from "./interfaces";
-import { PrismaService } from "../prisma/prisma.service";
-import { AuthService } from "./auth.service";
+import { Readable } from 'stream'
+import { mockPrisma, mockUser } from '../../test/mocks'
+import { AuthController } from './auth.controller'
 
-import { AuthController } from "./auth.controller";
-import { mockPrisma, mockUser } from "../../test/mocks";
+jest.mock('bcrypt', () => ({
+  hash: jest.fn().mockResolvedValue('hashedPassword')
+}))
 
-jest.mock("bcrypt", () => ({
-  hash: jest.fn().mockResolvedValue("hashedPassword"),
-}));
-
-describe("AuthService", () => {
-  let authService: AuthService;
+describe('AuthService', () => {
+  let authService: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,98 +29,219 @@ describe("AuthService", () => {
         AuthService,
         {
           provide: PrismaService,
-          useValue: {
-            mockPrisma,
-            update: jest.fn(),
-          },
-        },
+          useValue: mockPrisma
+        }
       ],
-      imports: [PassportModule.register({ defaultStrategy: "jwt" })],
-    }).compile();
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })]
+    }).compile()
 
-    authService = module.get<AuthService>(AuthService);
-    mockPrisma.user.create.mockClear();
-  });
+    authService = module.get<AuthService>(AuthService)
+    mockPrisma.user.create.mockClear()
+  })
 
-  describe("create user", () => {
+  describe('create user', () => {
     beforeEach(async () => {
-      const errors = await validate(mockUser);
-      expect(errors.length).toBe(0);
-    });
+      const errors = await validate(mockUser)
+      expect(errors.length).toBe(0)
+    })
 
-    it("should return a new user", async () => {
-      const { user } = await authService.create(mockUser);
+    it('should return a new user', async () => {
+      const { user } = await authService.create(mockUser)
 
-      expect(mockPrisma.user.create).toHaveBeenCalledTimes(1);
+      expect(mockPrisma.user.create).toHaveBeenCalledTimes(1)
       expect(user).toMatchObject({
         id: expect.any(String),
         name: mockUser.name,
         email: mockUser.email,
         phoneNumber: mockUser.phoneNumber,
-        role: "CLIENT",
-      });
+        role: 'CLIENT'
+      })
 
-      expect(user).not.toHaveProperty("password");
-    });
+      expect(user).not.toHaveProperty('password')
+    })
 
-    it("should return a registered user conflict error exception", async () => {
-      mockPrisma.user.findUnique.mockResolvedValue(mockUser);
+    it('should return a registered user conflict error exception', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(mockUser)
 
       await expect(authService.create(mockUser)).rejects.toThrow(
         new ConflictException(
           `User with email ${mockUser.email} is already registered`
         )
-      );
-    });
-  });
+      )
+    })
+  })
 
-  describe("renew token", () => {
-    it("shoult return an user with new token", async () => {
+  describe('renew token', () => {
+    it('shoult return an user with new token', async () => {
       const newMockUser: User = {
-        id: "randomUUID",
+        id: 'randomUUID',
         email: mockUser.email,
         name: mockUser.name,
         phoneNumber: mockUser.phoneNumber,
-        role: "CLIENT",
-      };
+        role: 'CLIENT'
+      }
 
-      const { token, user } = await authService.renewToken(newMockUser);
+      const { token, user } = await authService.renewToken(newMockUser)
 
-      expect(user).toEqual(newMockUser);
-      expect(typeof token).toBe("string");
-    });
-  });
+      expect(user).toEqual(newMockUser)
+      expect(typeof token).toBe('string')
+    })
+  })
 
-  describe("Update User Info", () => {
-    it("should return updates user data", async () => {
-      const mockId = "1";
-      const mockUser = {
-        name: "sam",
-        email: "sam@email.com",
-        phone_number: "5610747645",
-        avatar: "https://algo.com/algo.png",
-      };
+  describe('Update User Info', () => {
+    const mockId: string = '1'
+    const mockUser: {
+      name: string
+      email: string
+      phone_number: string
+      is_active: boolean
+      is_verified: boolean
+    } = {
+      name: 'sam',
+      email: 'sam@email.com',
+      phone_number: '5610747645',
+      is_active: false,
+      is_verified: false
+    }
 
-      jest.spyOn(authService, "update").mockResolvedValue(mockUser);
-      await expect(authService.update(mockId, mockUser)).resolves.toEqual(
-        mockUser
-      );
-    });
+    it('should return updates user data', async () => {
+      const mockId = 'someUserId'
 
-    it("should throw notFoundExpection whe the user not exist", async () => {
-      const mockId = "999";
-      const mockUser = {
-        name: "sam",
-        email: "sam@email.com",
-        phone_number: "5610747645",
-        avatar: "https://algo.com/algo.png",
-      };
-      jest
-       .spyOn(authService, "update")
-       .mockRejectedValue(new NotFoundException("User doesn't exist")); // Use NotFoundException directly
+      const updatedData = {
+        name: 'samuel',
+        email: 'samuel@hotmail.com',
+        phone_number: '1234567890'
+      }
+
+      mockUser.is_active = true
+      mockUser.is_verified = true
+
+      mockPrisma.user.findUnique.mockResolvedValue({ id: mockId, ...mockUser })
+      mockPrisma.user.update.mockResolvedValue(updatedData)
+
+      const result = await authService.update(mockId, updatedData)
+      expect(result).toEqual(updatedData)
+    })
+
+    it('should throw notFoundExpection whe the user not exist', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(undefined)
       await expect(authService.update(mockId, mockUser)).rejects.toThrow(
         NotFoundException
-      );
-    });
-  });
-});
+      )
+    })
+
+    it('should throw UnauthorizedException when the user is inactive', async () => {
+      const inactiveUser = { ...mockUser, is_active: false }
+      mockPrisma.user.findUnique.mockResolvedValue(inactiveUser)
+
+      await expect(authService.update(mockId, mockUser)).rejects.toThrow(
+        UnauthorizedException
+      )
+    })
+
+    it('should throw UnauthorizedException when the user is Unverified', async () => {
+      const unveriedUser = { ...mockUser, is_verified: false }
+      mockPrisma.user.findUnique.mockResolvedValue(unveriedUser)
+
+      await expect(authService.update(mockId, mockUser)).rejects.toThrow(
+        UnauthorizedException
+      )
+    })
+  })
+
+  describe('update-avatar', () => {
+    const mockId: string = '1'
+    const mockAvatar: { avatar: string } = {
+      avatar: 'https://res.cloudinary.com/dlfokylll/image/upload/v1717106810/x8iq5mmfuf802a2iux0k.jpg'
+    }
+    const mockFile: Express.Multer.File = {
+      fieldname: 'avatar',
+      originalname: 'example.jpg',
+      encoding: '7bit',
+      mimetype: 'image/jpeg',
+      size: 123456,
+      destination: '',
+      filename: 'example.jpg',
+      path: '',
+      buffer: null as unknown as Buffer,
+      stream: Readable.from([])
+    }
+
+    it('should update the user avatar correctly', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue({
+        id: mockId,
+        avatar: mockAvatar.avatar,
+        is_active: true,
+        is_verified: true
+      })
+
+      // se usa spyOn para simular el servicio de cloudinary en Ok
+      jest.spyOn(authService, 'updateAvatar').mockResolvedValue({
+        avatar: mockAvatar.avatar
+      })
+
+      await expect(authService.updateAvatar(mockId, mockFile)).resolves.toEqual(
+        {
+          avatar: mockAvatar.avatar
+        }
+      )
+    })
+
+    it('should fail to upload the user avatar', async () => {
+      // se usa spyOn para simular el servicio de cloudinary en fallo
+      jest.spyOn(authService, 'updateAvatar').mockImplementation(() => {
+        throw new Error('Failed to upload file')
+      })
+
+      try {
+        await authService.updateAvatar(mockId, mockFile)
+        expect(true).toBe(false)
+      } catch (error) {
+        expect(error.message).toBe('Failed to upload file')
+      }
+    })
+
+    it('should throw notFoundExpection whe the user not exist', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(undefined)
+      await expect(authService.updateAvatar(mockId, mockFile)).rejects.toThrow(
+        NotFoundException
+      )
+    })
+
+    it('should throw UnauthorizedException when the user is inactive', async () => {
+      const inactiveUser = { ...mockUser, is_verified: false }
+      mockPrisma.user.findUnique.mockResolvedValue(inactiveUser)
+
+      await expect(authService.updateAvatar(mockId, mockFile)).rejects.toThrow(
+        UnauthorizedException
+      )
+    })
+
+    it('should throw UnauthorizedException when the user is Unverified', async () => {
+      const unveriedUser = { ...mockUser, is_verified: false }
+      mockPrisma.user.findUnique.mockResolvedValue(unveriedUser)
+
+      await expect(authService.updateAvatar(mockId, mockFile)).rejects.toThrow(
+        UnauthorizedException
+      )
+    })
+  })
+
+  describe('forgot password', () => {
+    const mockForgotPassDto = {
+      email: mockUser.email
+    }
+    it('should return an error if no user was found with the same email', async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(null)
+      await expect(
+        authService.forgotPassword(mockForgotPassDto)
+      ).rejects.toThrow(new ConflictException('User not found'))
+    })
+
+    it("should return 200 if the user's email is found", async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(mockUser)
+      const result = await authService.forgotPassword(mockForgotPassDto)
+      expect(result).toEqual('Email succssfully sent')
+    })
+  })
+})
