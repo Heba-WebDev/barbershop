23dc343ea0895e45299f63aadf9c3298c5a5a79e
PabloLopez23 <pabloexelopezh@gmail.com>
2024-06-01 20:05:30 -0300

    login formulario2

--- frontend/src/pages/login/loginView.tsx
+++ frontend/src/pages/login/loginView.tsx
@@ -1,36 +1,44 @@
 import React, { useState } from 'react';
 
 export const LoginView = () => {
-const [ value, setValue] = useState ({
-  email: '',
-  password: ''
-});
-const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setValue({ ...value, [event.target.value]: event.target.value });
-};
-const handleSumbit = () => {
-  console.log(value)
-}
-  return (
-   <form className=''>
-    <div className='flex flex-col gap-2'>
-    <label>Correo</label>
-    <input
-    type="email"
-    value={value.email}
-    onChange={handleInputChange}
-    placeholder= "Enter your username" />
-    </div>
+  const [value, setValue] = useState({
+    email: '',
+    password: ''
+  });
 
-    <div className='flex flex-col gap-2'>
-    <label>password</label>
-    <input
-    type="password"
-    value={value.password}
-    onChange={handleInputChange}
-    placeholder= "Enter your password" />
-    </div>
-    <button type='submit' onSubmit={handleSumbit}>Login</button>
-  </form>
-  )
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({ ...value, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(value);
+  }
+
+  return (
+    <form className='' onSubmit={handleSubmit}>
+      <div className='flex flex-col gap-2'>
+        <label htmlFor="email">Correo</label>
+        <input
+          name="email"
+          type="email"
+          value={value.email}
+          onChange={handleInputChange}
+          placeholder="Enter your email"
+        />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <label htmlFor="password">Contraseña</label>
+        <input
+          name="password"
+          type="password"
+          value={value.password}
+          onChange={handleInputChange}
+          placeholder="Enter your password"
+        />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  );
 }
