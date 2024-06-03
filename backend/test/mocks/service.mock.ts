c3a75374cdd7778079f1aeb4e3df4589938cccfa
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-03 16:33:59 -0400

    fix: update mock, remove id_user

--- backend/test/mocks/service.mock.ts
+++ backend/test/mocks/service.mock.ts
@@ -2,7 +2,6 @@ import { type CreateServiceDto } from 'src/service/dto/create-service.dto'
 
 export const serviceMock: CreateServiceDto = {
   name: 'new service',
-  price: 1000,
-  // avatar: '',
-  user_id: 'e3d23197-19a8-4878-8c33-cf834c6b9e88'
+  price: 1000
+  // avatar: ''
 }
