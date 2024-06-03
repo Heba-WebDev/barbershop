dccfc2237575024a7db47d6a74029e5f2f9fb26d
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-03 16:32:30 -0400

    fix: addes new param and us documentation swagger

--- backend/src/service/service.controller.ts
+++ backend/src/service/service.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Post, Body } from '@nestjs/common'
 import { ServiceService } from './service.service'
 import { CreateServiceDto } from './dto/create-service.dto'
-import { ApiTags } from '@nestjs/swagger'
-import { Auth } from '../auth/decorators'
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { Auth, GetUser } from '../auth/decorators'
+import { User } from 'src/auth/interfaces'
 
 @ApiTags('Service')
 @Controller('service')
 export class ServiceController {
   constructor (private readonly serviceService: ServiceService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+      'This endpoint needs a bearear token to extract the user from the request'
+  })
   @Post()
   @Auth('OWNER')
-  async create (@Body() createServiceDto: CreateServiceDto) {
-    return await this.serviceService.create(createServiceDto)
+  async create (@Body() createServiceDto: CreateServiceDto, @GetUser() user: User) {
+    return await this.serviceService.create(createServiceDto, user)
   }
 }
