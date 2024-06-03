ac77d790cd81b93fd91a1322b9f185a18ced0cd9
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-03 16:31:54 -0400

    fix: remove id_user and add api property for swagger

--- backend/src/service/dto/create-service.dto.ts
+++ backend/src/service/dto/create-service.dto.ts
@@ -1,13 +1,23 @@
-import { IsNumber, IsString, IsUUID } from 'class-validator'
-import { UUID } from 'crypto'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNumber, IsString } from 'class-validator'
+// import { UUID } from 'crypto'
 
 export class CreateServiceDto {
+  @ApiProperty({
+    example: 'cut hair',
+    description: 'service - name',
+    nullable: false,
+    minLength: 1
+  })
   @IsString()
     name: string
 
+  @ApiProperty({
+    example: '7000',
+    description: 'service - price',
+    nullable: false,
+    minLength: 1
+  })
   @IsNumber()
     price: number
-
-  @IsUUID()
-    user_id: UUID
 }
