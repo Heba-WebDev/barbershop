5b8a2a360d5564a0d4247cfa795039025bbb62f7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-04 00:24:47 +0300

    test: imported button from shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import "./App.css";
+import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
-  return (
-    <>
-      <h1 className=" text-2xl text-blue-600">Hola Mundo!!</h1>
-    </>
-  );
+    return (
+        <>
+            <Button className=' px-6 h-8 rounded-none'>Holaaa</Button>
+            <h1 className=" text-2xl text-blue-600">Hola Mundo!!</h1>
+        </>
+    )
 }
 
-export default App;
+export default App
