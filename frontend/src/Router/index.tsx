8b33590cdc66e2efcb6f041855abfa5d7cbe23d6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-04 22:16:09 +0300

    fix: eslint

--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -1,39 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "../App";
-import { HomeView, RegisterView } from "../pages";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from '../App'
+import { HomeView, RegisterView } from '../pages'
 
 interface HomeData{
   message:string;
 }
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    loader: () => ({ message: "Hello Data Router!" }),
-    Component() {
-      return <App />;
+    {
+        path: '/',
+        loader: () => ({ message: 'Hello Data Router!' }),
+        Component() {
+            return <App />
+        },
     },
-  },
-  {
-    path: "/register",
-    loader: () => ({ message: "Hello Register!" }),
-    Component() {
-      return <RegisterView />;
+    {
+        path: '/register',
+        loader: () => ({ message: 'Hello Register!' }),
+        Component() {
+            return <RegisterView />
+        },
     },
-  },
-  {
-    path:"/home",
-    loader:():HomeData=>({message:"Hello Home!"}),
-    Component(){
-      return <HomeView/>
-    }
-  },
-]);
+    {
+        path:'/home',
+        loader:():HomeData=>({message:'Hello Home!'}),
+        Component(){
+            return <HomeView/>
+        }
+    },
+])
 
 export default function Router() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+    return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
 }
 
 if (import.meta.hot) {
-  import.meta.hot.dispose(() => router.dispose());
+    import.meta.hot.dispose(() => router.dispose())
 }
