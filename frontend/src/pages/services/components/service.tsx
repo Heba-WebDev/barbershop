133a1c32f72bb69be1886cb0eec02095c6cf95ce
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-06 00:36:04 +0300

    fix: removed console log and fixed eslint issues

--- frontend/src/pages/services/components/service.tsx
+++ frontend/src/pages/services/components/service.tsx
@@ -1,16 +1,15 @@
 import { service } from '../types'
 import { Switch } from '@/components/ui/switch'
 
-export const Service = ({id, name, company_id, price, is_active}: service) => {
-    console.log(id, company_id, is_active)
+export const Service = ({name, price}: service) => {
     return (
-        <div className=" flex items-center justify-between">
+        <div className='flex items-center justify-between'>
             <div>
-                <p className="md:text-xl">{name}</p>
+                <p className='md:text-xl'>{name}</p>
                 <p className=' text-sm text-slate-400'>${price}</p>
             </div>
             <div>
-                <Switch className="data-[state]:checked:bg-red-600" />
+                <Switch className='data-[state]:checked:bg-red-600' />
             </div>
         </div>
     )
