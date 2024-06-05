7dd4b5c68dbd3f7abbb7404151a148ed46a2deb0
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-05 19:04:11 +0300

    fix: eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,10 @@ import { Button } from '@/components/ui/button'
 function App() {
 
     return (
-            <>
-                <Button className=' px-6 h-8 rounded-none'>Hola</Button>
-                <h1 className=" text-2xl text-blue-600">Hola Mundow!!</h1>
-            </>
+        <>
+            <Button className=' px-6 h-8 rounded-none'>Hola</Button>
+            <h1 className=" text-2xl text-blue-600">Hola Mundow!!</h1>
+        </>
         
     )
 }
