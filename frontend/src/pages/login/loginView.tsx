0c5a15eebf78f75c27b3042f39db9c44947a83ae
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-05 05:00:02 +0300

    fix: merge conflicts

--- frontend/src/pages/login/loginView.tsx
+++ frontend/src/pages/login/loginView.tsx
@@ -1,6 +1,36 @@
+import React, { useState } from 'react';
 
 export const LoginView = () => {
+const [ value, setValue] = useState ({
+  email: '',
+  password: ''
+});
+const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setValue({ ...value, [event.target.value]: event.target.value });
+};
+const handleSumbit = () => {
+  console.log(value)
+}
   return (
-   <h1>Login View</h1>
+   <form className=''>
+    <div className='flex flex-col gap-2'>
+    <label>Correo</label>
+    <input
+    type="email"
+    value={value.email}
+    onChange={handleInputChange}
+    placeholder= "Enter your username" />
+    </div>
+
+    <div className='flex flex-col gap-2'>
+    <label>password</label>
+    <input
+    type="password"
+    value={value.password}
+    onChange={handleInputChange}
+    placeholder= "Enter your password" />
+    </div>
+    <button type='submit' onSubmit={handleSumbit}>Login</button>
+  </form>
   )
 }
