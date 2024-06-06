f4825ea98f4bb939b68e7ce5cff7b9c004d6dbf4
isakiDev <gaspar.c.developer@gmail.com>
2024-06-06 05:08:27 -0400

    chore: delete unnecessary code

--- backend/src/schedule/schedule.controller.ts
+++ backend/src/schedule/schedule.controller.ts
@@ -1,40 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Body, Controller, Get, Param, ParseUUIDPipe, Patch } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+
 import { ScheduleService } from './schedule.service'
-import { CreateScheduleDto } from './dto/create-schedule.dto'
-// import { UpdateScheduleDto } from './dto/update-schedule.dto'
 import { Auth, GetUser } from '../auth/decorators'
 import { User } from '../auth/interfaces'
+import { UpdateScheduleDto } from './dto'
 
+@ApiTags('Schedule')
 @Controller('schedule')
 export class ScheduleController {
-  constructor (private readonly scheduleService: ScheduleService) {}
+  constructor (private readonly scheduleService: ScheduleService) { }
 
-  @Post()
-  @Auth()
-  async create (
-  @GetUser() user: User,
-    @Body() createScheduleDto: CreateScheduleDto
-  ) {
-    // return await this.scheduleService.create(user, createScheduleDto)
+  @Get(':companyId')
+  async findAllByCompanyId (
+  @Param('companyId', ParseUUIDPipe) companyId: string) {
+    return await this.scheduleService.findAllByCompanyId(companyId)
   }
 
-  // @Get()
-  // findAll () {
-  //   return this.scheduleService.findAll()
-  // }
-
-  // @Get(':id')
-  // findOne (@Param('id') id: string) {
-  //   return this.scheduleService.findOne(+id)
-  // }
-
-  // @Patch(':id')
-  // update (@Param('id') id: string, @Body() updateScheduleDto: UpdateScheduleDto) {
-  //   return this.scheduleService.update(+id, updateScheduleDto)
-  // }
-
-  // @Delete(':id')
-  // remove (@Param('id') id: string) {
-  //   return this.scheduleService.remove(+id)
-  // }
+  @Patch(':id')
+  @Auth('OWNER')
+  async update (
+  @Param('id') id: string,
+    @GetUser() user: User,
+    @Body() updateScheduleDto: UpdateScheduleDto) {
+    return await this.scheduleService.update(+id, user.id, updateScheduleDto)
+  }
 }
--- backend/src/schedule/schedule.module.ts
+++ backend/src/schedule/schedule.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+
 import { ScheduleService } from './schedule.service'
 import { ScheduleController } from './schedule.controller'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Module({
   controllers: [ScheduleController],
