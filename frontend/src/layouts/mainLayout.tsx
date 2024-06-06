add1a2602ac0a2e930af682ca1f8f1045875a02a
hallzyx <hallinxsl7@gmail.com>
2024-06-05 20:32:47 -0500

    feat: BottomNavBar added.

--- frontend/src/layouts/mainLayout.tsx
+++ frontend/src/layouts/mainLayout.tsx
@@ -1,9 +1,11 @@
+import { BottomNavBar } from '@/components/globals/BottomNavBar'
 import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
     return (
         <div className='p-2 container mx-auto'>
             <Outlet />
+            <BottomNavBar />
         </div>
     )
 }
