0e0854773add85a73b6af9a72045498914313859
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-06 19:01:53 -0400

    fix: ,

--- backend/src/service/service.module.ts
+++ backend/src/service/service.module.ts
@@ -3,12 +3,13 @@ import { ServiceService } from './service.service'
 import { ServiceController } from './service.controller'
 import { PrismaModule } from '../prisma/prisma.module'
 import { AuthModule } from '../auth/auth.module'
-import { CompanyService } from '../company/company.service'
+// import { CompanyService } from '../company/company.service'
+import { CompanyModule } from '../company/company.module'
 
 @Module({
   controllers: [ServiceController],
   providers: [ServiceService],
-  imports: [PrismaModule, AuthModule, CompanyService],
+  imports: [PrismaModule, AuthModule, CompanyModule],
   exports: [ServiceService]
 })
 export class ServiceModule {}
