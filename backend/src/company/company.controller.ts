00b14f50c6858d09fa41d32d52d6611e5fac8158
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-05 22:32:19 -0400

    feat: new request post for create new company

--- backend/src/company/company.controller.ts
+++ backend/src/company/company.controller.ts
@@ -1,34 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CompanyService } from './company.service';
-import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
+import { Controller, Post, Body } from '@nestjs/common'
+import { CompanyService } from './company.service'
+import { CreateCompanyDto } from './dto/create-company.dto'
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger'
+import { Auth, GetUser } from '../auth/decorators'
+import { User } from '../auth/interfaces/user.interface'
 
 @Controller('company')
 export class CompanyController {
-  constructor(private readonly companyService: CompanyService) {}
+  constructor (private readonly companyService: CompanyService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+    'This endpoint needs a bearear token to extract the user from the request'
+  })
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companyService.create(createCompanyDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.companyService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companyService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companyService.update(+id, updateCompanyDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companyService.remove(+id);
+  @Auth('CLIENT')
+  async create (@Body() createCompanyDto: CreateCompanyDto, @GetUser() user: User) {
+    return await this.companyService.create(createCompanyDto, user)
   }
 }
