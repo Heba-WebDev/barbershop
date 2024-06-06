13a23dd00ff07f11b41082f10fd226ae09c2c770
isakiDev <gaspar.c.developer@gmail.com>
2024-06-06 17:31:49 -0400

    feat: convert value date to valid date

--- backend/test/mocks/schedule.mock.ts
+++ backend/test/mocks/schedule.mock.ts
@@ -1,10 +1,12 @@
+import { $Enums } from '@prisma/client'
+
 export const mockSchedule = {
-  id: 4,
-  day: 'MONDAY',
-  initial_start_date: '1970-01-01T09:00:00Z',
-  initial_end_date: '1970-01-01T13:00:00Z',
-  final_start_date: '1970-01-01T14:00:00Z',
-  final_end_date: '1970-01-01T19:00:00Z',
+  id: 2,
+  day: $Enums.Day.MONDAY,
+  initial_start_date: new Date('1970-01-01T09:00:00Z'),
+  initial_end_date: new Date('1970-01-01T13:00:00Z'),
+  final_start_date: new Date('1970-01-01T14:00:00Z'),
+  final_end_date: new Date('1970-01-01T19:00:00Z'),
   state: true,
   interval: 30
 }
