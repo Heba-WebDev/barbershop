15956e073a55f9b9660a545485a234eb100bb408
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-05 22:32:46 -0400

    feat: new imports

--- backend/src/company/company.module.ts
+++ backend/src/company/company.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { CompanyService } from './company.service';
-import { CompanyController } from './company.controller';
+import { Module } from '@nestjs/common'
+import { CompanyService } from './company.service'
+import { CompanyController } from './company.controller'
+import { AuthModule } from 'src/auth/auth.module'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   controllers: [CompanyController],
   providers: [CompanyService],
+  imports: [PrismaModule, AuthModule]
 })
 export class CompanyModule {}
