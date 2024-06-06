c3bbe298c13c75109867e8a3b14c3f33c99d5173
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-05 22:31:13 -0400

    feat: new conditional error

--- backend/src/common/utils/handleErrorsExcepcions.ts
+++ backend/src/common/utils/handleErrorsExcepcions.ts
@@ -1,8 +1,9 @@
-import { InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { ConflictException, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
 
 export const handleErrorExceptions = (error: unknown) => {
   if (error instanceof NotFoundException) throw new NotFoundException(error.message)
   if (error instanceof UnauthorizedException) throw new UnauthorizedException(error.message)
+  if (error instanceof ConflictException) throw new ConflictException(error.message)
 
   throw new InternalServerErrorException('An unexpected error occurred')
 }
