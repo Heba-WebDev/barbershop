86342154a464904784e33d981ec979b37e0348d1
isakiDev <gaspar.c.developer@gmail.com>
2024-06-06 05:11:24 -0400

    feat: add new properties to validate

--- backend/src/schedule/dto/update-schedule.dto.ts
+++ backend/src/schedule/dto/update-schedule.dto.ts
@@ -1,17 +1,66 @@
-import { IsEnum, IsISO8601, IsOptional } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsISO8601, IsIn, IsOptional } from 'class-validator'
 
-import { $Enums } from '@prisma/client'
+const INTERVALS = [30, 45, 60]
 
 export class UpdateScheduleDto {
-  @IsEnum($Enums.Day)
-  @IsOptional()
-  readonly day: $Enums.Day
-
+  @ApiProperty({
+    description: 'Schedule - Initial start date',
+    example: '1970-01-01T01:00:00Z',
+    nullable: true,
+    type: Date
+  })
   @IsISO8601()
   @IsOptional()
-  readonly start_date: Date
+  readonly initial_start_date?: Date
 
+  @ApiProperty({
+    description: 'Schedule - Initial end date',
+    example: '1970-01-01T01:00:00Z',
+    nullable: true,
+    type: Date
+  })
   @IsISO8601()
   @IsOptional()
-  readonly end_date: Date
+  readonly initial_end_date?: Date
+
+  @ApiProperty({
+    description: 'Schedule - Final start date',
+    example: '1970-01-01T01:00:00Z',
+    nullable: true,
+    type: Date
+  })
+  @IsISO8601()
+  @IsOptional()
+  readonly final_start_date?: Date
+
+  @ApiProperty({
+    description: 'Schedule - Final end date',
+    example: '1970-01-01T01:00:00Z',
+    nullable: true,
+    type: Date
+  })
+  @IsISO8601()
+  @IsOptional()
+  readonly final_end_date?: Date
+
+  @ApiProperty({
+    description: 'Schedule - Initial start date',
+    example: true,
+    nullable: true,
+    type: Boolean
+  })
+  @IsBoolean()
+  @IsOptional()
+  readonly state?: boolean
+
+  @ApiProperty({
+    description: 'Schedule - Interval',
+    example: 30,
+    nullable: true,
+    type: Number
+  })
+  @IsIn(INTERVALS, { message: 'The interval must be one of: $constraint1' })
+  @IsOptional()
+  readonly interval?: number
 }
