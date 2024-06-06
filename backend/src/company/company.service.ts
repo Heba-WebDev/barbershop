ff28d140cba4edc4451919d3cb4fd8e0250c3076
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-05 22:33:56 -0400

    feat: new functions for company

--- backend/src/company/company.service.ts
+++ backend/src/company/company.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { type CreateCompanyDto } from './dto/create-company.dto'
+import { type User } from '../auth/interfaces'
+import { handleErrorExceptions } from '../common/utils'
+import { PrismaService } from '../prisma/prisma.service'
+import { AuthService } from '../auth/auth.service'
+import { type UUID } from 'crypto'
 
 @Injectable()
 export class CompanyService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+  constructor (
+    private readonly prisma: PrismaService,
+    private readonly auth: AuthService
+  ) {}
+
+  async create (createCompanyDto: CreateCompanyDto, user: User) {
+    try {
+      await this.companyExist(user.id as UUID)
+
+      const company = await this.prisma.company.create({
+        data: {
+          ...createCompanyDto,
+          user_id: user.id
+        },
+        select: {
+          address: true,
+          avatar: true,
+          name: true,
+          phone_number: true
+        }
+      })
+
+      if (company) await this.auth.changeRole(user.id as UUID)
+
+      return company
+    } catch (error) {
+      handleErrorExceptions(error)
+    }
+
+    return 'This action adds a new company'
   }
 
-  findAll() {
-    return `This action returns all company`;
+  async companyExist (userID: UUID) {
+    try {
+      const exist = await this.prisma.company.findFirst({ where: { user_id: userID } })
+
+      if (exist) throw new ConflictException('Company Exist')
+
+      return false
+    } catch (error) {
+      handleErrorExceptions(error)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
-  }
+  async findCompanyForOwner (userID: UUID) {
+    try {
+      const company = await this.prisma.company.findFirst({
+        where: { user_id: userID }
+      })
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
-  }
+      if (!company) throw new NotFoundException('Company not exist')
+      if (!company.is_active) throw new UnauthorizedException('Company is Inactive')
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+      return company
+    } catch (error) {
+      handleErrorExceptions(error)
+    }
   }
 }
