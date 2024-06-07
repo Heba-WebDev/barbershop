cb08aed83b2addd81240836b081d7d4b3b5e5b7d
Ilce <ilsemilena1706@gmail.com>
2024-06-07 12:49:32 -0500

    feat: add mock

--- frontend/src/pages/hours/dateDay.tsx
+++ frontend/src/pages/hours/dateDay.tsx
@@ -11,14 +11,17 @@ type DateDayProps = {
 export const DateDay = ({ day, dateHour }: DateDayProps) => {
   return (
     <div className="flex justify-between items-center">
-      <h3 className="font-medium">{day}</h3>
-      <span className="bg-dark-gray px-3 rounded-xl">{dateHour}</span>
-      <div>
+      <h3 className="font-medium w-10">{day.slice(0, 3)}</h3>
+      <div className="">
+     
+        <span className="bg-dark-gray px-3 rounded-xl">{dateHour}</span>
+      </div>
+      <div className="">
         <button className="bg-dark-gray px-3 rounded-xl text-light-cayn text-3xl">
           +
         </button>
       </div>
-      <div>
+      <div className="">
         <Switch className="data-[state]:checked:bg-red-600" />
       </div>
     </div>
