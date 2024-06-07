969035227d877c6ddcf706d1f85119bc1e2a6eca
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-06 21:41:54 -0400

    chore: update imports

--- backend/src/appointment/appointment.module.ts
+++ backend/src/appointment/appointment.module.ts
@@ -4,10 +4,11 @@ import { AppointmentController } from './appointment.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { AuthModule } from 'src/auth/auth.module'
 import { ServiceModule } from 'src/service/service.module'
+import { CompanyModule } from 'src/company/company.module'
 
 @Module({
   controllers: [AppointmentController],
   providers: [AppointmentService],
-  imports: [PrismaModule, AuthModule, ServiceModule]
+  imports: [PrismaModule, AuthModule, ServiceModule, CompanyModule]
 })
 export class AppointmentModule {}
