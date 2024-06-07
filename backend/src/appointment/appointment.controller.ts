cf2edd763582ac733c21528fd1e3ad6c79a22e15
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-06 21:39:27 -0400

    chore: update documentation swagger and add authetication

--- backend/src/appointment/appointment.controller.ts
+++ backend/src/appointment/appointment.controller.ts
@@ -1,15 +1,23 @@
 import { Controller, Post, Body } from '@nestjs/common'
 import { AppointmentService } from './appointment.service'
 import { CreateAppointmentDto } from './dto/create-appointment.dto'
-import { Auth } from '../auth/decorators'
+import { Auth, GetUser } from '../auth/decorators'
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { User } from '../auth/interfaces'
 
+@ApiTags('Appointment')
 @Controller('appointment')
 export class AppointmentController {
   constructor (private readonly appointmentService: AppointmentService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+    'This endpoint needs a bearear token to extract the user from the request'
+  })
   @Post()
-  @Auth()
-  async create (@Body() createAppointmentDto: CreateAppointmentDto) {
-    return await this.appointmentService.create(createAppointmentDto)
+  @Auth('CLIENT')
+  async create (@Body() createAppointmentDto: CreateAppointmentDto, @GetUser() user: User) {
+    return await this.appointmentService.create(createAppointmentDto, user)
   }
 }
