c0ad611fe92d43d648ac31024b83a8178ad0313d
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-08 17:41:04 -0400

    feat: new request get for get all employee

--- backend/src/company/company.controller.ts
+++ backend/src/company/company.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, Get, Param, ParseUUIDPipe } from '@nestjs/common'
 import { CompanyService } from './company.service'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Auth, GetUser } from '../auth/decorators'
 import { User } from '../auth/interfaces/user.interface'
+import { UUID } from 'crypto'
 
 @ApiTags('Company')
 @Controller('company')
 export class CompanyController {
   constructor (private readonly companyService: CompanyService) {}
+
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+    'This endpoint needs a bearear token to extract the user from the request'
+  })
+  @Get('employees/:id')
+  @Auth('CLIENT', 'EMPLOYEE', 'OWNER')
+  async getAllEmployee (@Param('id', ParseUUIDPipe) companyID: UUID) {
+    return await this.companyService.getAllEmployee(companyID)
+  }
 
   @ApiBearerAuth()
   @ApiOperation({
