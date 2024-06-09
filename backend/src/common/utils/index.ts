49c10a76edacb42aa16d6d346d64a909db981a3c
isakiDev <gaspar.c.developer@gmail.com>
2024-06-09 03:14:22 -0400

    feat: add sendResetPassword in emailService

--- backend/src/common/utils/index.ts
+++ backend/src/common/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './handleErrorsExcepcions'
+export * from './generateJwt'
--- backend/src/email/email.service.ts
+++ backend/src/email/email.service.ts
@@ -2,29 +2,45 @@ import { MailerService } from '@nestjs-modules/mailer'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 
-import { type SendUserConfirmationDto } from './dto'
+import { type EmailConfirmationDto } from './dto'
+import { generateJwt } from '../common/utils'
 
 @Injectable()
 export class EmailService {
+  private readonly logo: string = 'https://res.cloudinary.com/dvzycdk7y/image/upload/v1717911007/barber-hub/logo/cj2ghikqwbc3wnyo0kt8.png'
+  private readonly baseUrl: string = this.configService.get('FRONT_END_HOST')
+  private readonly appName: string = 'BarberHub'
+
   constructor (
     private readonly mailerService: MailerService,
     private readonly configService: ConfigService
   ) { }
 
-  async sendUserConfirmation (sendUserConfirmationDto: SendUserConfirmationDto) {
-    const { name, email, token } = sendUserConfirmationDto
+  async sendUserConfirmation (emailConfirmationDto: EmailConfirmationDto) {
+    const { name, email, token } = emailConfirmationDto
 
-    const baseUrl = this.configService.get('FRONT_END_HOST')
     const subject = `Bienvenido ${name}`
-    const appName = 'BarberHub'
-    const url = `${baseUrl}?token=${token}`
-    const logo = 'https://res.cloudinary.com/dvzycdk7y/image/upload/v1717911007/barber-hub/logo/cj2ghikqwbc3wnyo0kt8.png'
+    const url = `${this.baseUrl}?token=${token}`
 
     await this.mailerService.sendMail({
       to: email,
       subject,
       template: './confirmation',
-      context: { name, url, appName, logo }
+      context: { name, url, appName: this.appName, logo: this.logo }
+    })
+  }
+
+  async sendResetPassword (userId: string, email: string, name: string) {
+    const token = await generateJwt({ id: userId })
+
+    const subject = 'Reiniciar contraseña | Barbería'
+    const url = `${this.baseUrl}/reset-password/${token}`
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject,
+      template: './reset-password',
+      context: { name, url, appName: this.appName, logo: this.logo }
     })
   }
 }
