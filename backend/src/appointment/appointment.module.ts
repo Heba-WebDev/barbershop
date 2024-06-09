ea4e2c9e9144cb0311a374264c1c3455cb8ec3f8
isakiDev <gaspar.c.developer@gmail.com>
2024-06-08 21:26:12 -0400

    refactor: delete unused code

--- backend/src/appointment/appointment.module.ts
+++ backend/src/appointment/appointment.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common'
 import { AppointmentService } from './appointment.service'
 import { AppointmentController } from './appointment.controller'
-import { PrismaModule } from 'src/prisma/prisma.module'
-import { AuthModule } from 'src/auth/auth.module'
-import { ServiceModule } from 'src/service/service.module'
-import { CompanyModule } from 'src/company/company.module'
+import { PrismaModule } from '../prisma/prisma.module'
+import { AuthModule } from '../auth/auth.module'
+import { ServiceModule } from '../service/service.module'
+import { CompanyModule } from '../company/company.module'
 
 @Module({
   controllers: [AppointmentController],
   providers: [AppointmentService],
-  imports: [PrismaModule, AuthModule, ServiceModule, CompanyModule]
+  imports: [PrismaModule, AuthModule, ServiceModule, CompanyModule],
+  exports: [AppointmentService]
 })
 export class AppointmentModule {}
