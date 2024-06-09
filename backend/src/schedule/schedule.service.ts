2c1469eca37b397348440fc9bb6bb0c256abebd0
isakiDev <gaspar.c.developer@gmail.com>
2024-06-08 21:39:51 -0400

    feat: add new methods to get available hours

--- backend/src/schedule/schedule.service.ts
+++ backend/src/schedule/schedule.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
 
-import { getTimeForDate } from '../common/utils'
+import { getTimeForDate, getHoursWithInterval, getFormatedHours, getNameDay } from './utils'
 import { PrismaService } from '../prisma/prisma.service'
 import { type QueryParamsScheduleDto, type UpdateScheduleDto } from './dto'
+import { AppointmentService } from '../appointment/appointment.service'
+import { type $Enums } from '@prisma/client'
 
 @Injectable()
 export class ScheduleService {
-  constructor (private readonly prismaService: PrismaService) {}
+  constructor (
+    private readonly prismaService: PrismaService,
+    private readonly appointmentService: AppointmentService
+  ) {}
 
   async update (id: number, userId: string, updateScheduleDto: UpdateScheduleDto) {
     const schedule = await this.findOneById(id)
@@ -27,7 +32,13 @@ export class ScheduleService {
 
     if (!schedule) throw new NotFoundException(`Schedule with id ${id} not found`)
 
-    return schedule
+    return {
+      ...schedule,
+      initial_start_date: getTimeForDate(schedule.initial_start_date),
+      initial_end_date: getTimeForDate(schedule.initial_end_date),
+      final_start_date: getTimeForDate(schedule.final_start_date),
+      final_end_date: getTimeForDate(schedule.final_end_date)
+    }
   }
 
   async findAllByCompanyId (querys: QueryParamsScheduleDto, companyId: string) {
@@ -46,6 +57,41 @@ export class ScheduleService {
     }))
 
     return schedulesFormated
+  }
+
+  async findOneByCompanyIdAndDay (companyId: string, day: $Enums.Day) {
+    const schedule = await this.prismaService.schedule.findFirst({ where: { company_id: companyId, day } })
+
+    if (!schedule) throw new NotFoundException(`Schedule with company id ${companyId} and day ${day} not found`)
+
+    return schedule
+  }
+
+  private async findHoursWithIntervalById (idSchedule: number) {
+    const {
+      initial_start_date: initialStartDate,
+      initial_end_date: initialEndDate,
+      final_start_date: finalStartDate,
+      final_end_date: finalEndDate,
+      interval
+    } = await this.findOneById(idSchedule)
+
+    const firstHours = getHoursWithInterval({ initialHour: initialStartDate, finalHour: initialEndDate, interval })
+    const lastHours = getHoursWithInterval({ initialHour: finalStartDate, finalHour: finalEndDate, interval })
+
+    if (!firstHours || !lastHours) throw new InternalServerErrorException()
+
+    return [...firstHours, ...lastHours]
+  }
+
+  async findAvailableHours (companyId: string, date: Date) {
+    const day = getNameDay(date.getDay())
+    const { id } = await this.findOneByCompanyIdAndDay(companyId, day)
+    const allPossibleHours = await this.findHoursWithIntervalById(id)
+
+    const allAvailableHours = await this.appointmentService.findAllAvailableHours(date, allPossibleHours)
+
+    return getFormatedHours(allAvailableHours)
   }
 
   // todo: move to company module
