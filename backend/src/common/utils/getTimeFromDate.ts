3ab6f374a486ca64610c4db802e45bb7083f7c8e
isakiDev <gaspar.c.developer@gmail.com>
2024-06-08 21:23:03 -0400

    chore: delete unused file

--- backend/src/common/utils/getTimeFromDate.ts
+++
@@ -1,6 +0,0 @@
-export const getTimeForDate = (date: Date) => {
-  const hours = date.getUTCHours().toString().padStart(2, '0')
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0')
-
-  return `${hours}:${minutes}`
-}
