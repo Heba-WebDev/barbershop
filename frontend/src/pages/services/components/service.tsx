2588a2d3f478406ca16af17cd00bd6df1409e282
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-11 01:44:17 +0300

    fix: update state after a service has been activated or desactivated

--- frontend/src/pages/services/components/service.tsx
+++ frontend/src/pages/services/components/service.tsx
@@ -1,14 +1,17 @@
 import { service } from '../types'
 import { Switch } from '@/components/ui/switch'
 import { toast } from 'react-toastify'
-import { desactivateServiceApi } from '../api'
+import { desactivateServiceApi, fetchServicsApi } from '../api'
 import { userState } from '@/state/user'
 
 export const Service = ({id, name, price, is_active}: service) => {
     const token = userState((state) => state.token)
+    const setServices = userState((state) => state.setService)
     const handleSwitch = async() => {
         try {
             await desactivateServiceApi(id, token as string)
+            const res = await fetchServicsApi(token as string)
+            setServices(res)
             toast.success(`El servicio ${name} ha sido desactivado`)
         }catch(error) {
             if (error instanceof Error) toast.error(error.message)
@@ -23,6 +26,7 @@ export const Service = ({id, name, price, is_active}: service) => {
             <div>
                 <Switch onClick={handleSwitch} checked={is_active} className='data-[state]:checked:bg-red-600' />
             </div>
+            
         </div>
     )
 }
