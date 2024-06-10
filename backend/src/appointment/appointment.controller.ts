0a9bae4bb2f6b1eb4bb045b296e00ce7f40b9ad3
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-06-10 17:03:36 -0400

    feat: new get controller

--- backend/src/appointment/appointment.controller.ts
+++ backend/src/appointment/appointment.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, Get, Query } from '@nestjs/common'
 import { AppointmentService } from './appointment.service'
 import { CreateAppointmentDto } from './dto/create-appointment.dto'
 import { Auth, GetUser } from '../auth/decorators'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { User } from '../auth/interfaces'
+import { FindAppointmentDto } from './dto/find-appointment.dto'
 
 @ApiTags('Appointment')
 @Controller('appointment')
 export class AppointmentController {
   constructor (private readonly appointmentService: AppointmentService) {}
+
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+    'This endpoint needs a bearear token to extract the user from the request'
+  })
+  @Get()
+  @Auth('CLIENT', 'EMPLOYEE', 'OWNER')
+  async get (@GetUser() user: User, @Query() findAppointmentDto: FindAppointmentDto) {
+    return await this.appointmentService.get(user, findAppointmentDto)
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({
+    description:
+    'This endpoint needs a bearear token to extract the user from the request'
+  })
+  @Get()
+  @Auth('OWNER')
+  async getAppointment (@GetUser() user: User) {
+    await this.appointmentService.getAppointmetForEmployee(user)
+  }
 
   @ApiBearerAuth()
   @ApiOperation({
