bb418728d96ff26d43b3fe1ee9bd7b30902168d9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-12 00:21:46 +0300

    fix: navbar shouldn't show if the the current path if / or /home

--- frontend/src/layouts/mainLayout.tsx
+++ frontend/src/layouts/mainLayout.tsx
@@ -1,11 +1,13 @@
 import { BottomNavBar } from '@/components/globals/BottomNavBar'
 import { Header } from '@/components/layout'
 import { Outlet } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 export const MainLayout = () => {
+    const location = useLocation()
     return (
         <div className='p-2 container mx-auto mb-12 md:mb-0'>
-             <Header />
+            {location.pathname !== '/' && location.pathname !== '/home' && <Header />}
             <Outlet />
             <BottomNavBar />
         </div>
