1efe08dd731b4bd10bfe3b5e0d81d6caa2f5d9de
hallzyx <hallinxsl7@gmail.com>
2024-06-12 13:44:08 -0500

    feat: a

--- frontend/src/components/layout/Header/Search.tsx
+++ frontend/src/components/layout/Header/Search.tsx
@@ -1,7 +1,33 @@
+import { api } from '@/axios'
+import { userState } from '@/state/user'
 import { FaSearch } from 'react-icons/fa'
 
 
+
+
+
 export const Search = () => {
+
+    const token=userState((store)=>store.token);
+
+    const callgetCompany=async()=>{
+        try{
+            const response=await api.get("/api/company",{
+                headers:{
+                    Authorization:token,
+                }
+            })
+            return response.data;
+        }catch(error){
+            
+        }
+        
+    }
+
+
+    const barberias=callgetCompany();
+    console.log(barberias)
+    console.log(token)
     return (
         <form className="flex">
             <input type="search" placeholder="Buscar barbería..."
