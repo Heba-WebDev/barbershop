a874fc6836426a8c5f60fa02fc54ca62bf2d5e34
Ilce <ilsemilena1706@gmail.com>
2024-06-12 21:41:33 -0500

    feat: add btn deleted

---
+++ frontend/src/assets/trashIcon.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+
+/* eslint-disable indent */
+type TrashIconProps ={
+    onClick: () => void;
+}
+export const TrashIcon: FC<TrashIconProps> = ({onClick}) => {
+
+  return (
+    <div  onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-trash"
+        width="27"
+        height="27"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 7l16 0" />
+        <path d="M10 11l0 6" />
+        <path d="M14 11l0 6" />
+        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+      </svg>
+    </div>
+  )
+}
--- frontend/src/pages/services/components/service.tsx
+++ frontend/src/pages/services/components/service.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable semi */
 import { service } from '../types'
 import { Switch } from '@/components/ui/switch'
 import { toast } from 'react-toastify'
-import { desactivateServiceApi, fetchServicsApi } from '../api'
+import {
+    desactivateServiceApi,
+    fetchServicsApi,
+    deletedServiceApi,
+} from '../api';
 import { userState } from '@/state/user'
+import { TrashIcon } from '@/assets/trashIcon'
 
 export const Service = ({id, name, price, is_active}: service) => {
     const token = userState((state) => state.token)
@@ -17,14 +23,31 @@ export const Service = ({id, name, price, is_active}: service) => {
             if (error instanceof Error) toast.error(error.message)
         }
     }
+ 
+    const handleDelete = async () => {
+        try {
+            await deletedServiceApi(id, token as string);
+            const res = await fetchServicsApi(token as string);
+            setServices(res);
+            toast.success(
+                `El servicio ${name} ha sido ${
+                    is_active ? 'desactivado' : 'activado'
+                }`
+            );
+        } catch (error) {
+            if (error instanceof Error) toast.error(error.message);
+        }
+    };
+
     return (
         <div className='flex items-center justify-between'>
             <div>
                 <p className='md:text-xl'>{name}</p>
                 <p className=' text-sm text-slate-400'>${price}</p>
             </div>
-            <div>
+            <div className='flex items-center gap-2'>
                 <Switch onClick={handleSwitch} checked={is_active} className='data-[state]:checked:bg-red-600' />
+                <TrashIcon onClick={handleDelete}/>
             </div>
             
         </div>
