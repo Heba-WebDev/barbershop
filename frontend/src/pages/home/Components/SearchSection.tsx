3f42543d014c279dc73600bcf4381a6ac1157c8d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-13 16:23:22 +0300

    fix: eslint

--- frontend/src/pages/home/Components/SearchSection.tsx
+++ frontend/src/pages/home/Components/SearchSection.tsx
@@ -1,5 +1,5 @@
-import { api } from '@/axios';
-import { useEffect, useRef, useState } from 'react';
+import { api } from '@/axios'
+import { useEffect, useRef, useState } from 'react'
 import { FaBell, FaSearch } from 'react-icons/fa'
 
 interface BarberiaBusqueda{
@@ -10,18 +10,18 @@ interface BarberiaBusqueda{
 
 export const SearchSection = () => {
 
-    const searchRef = useRef<HTMLDivElement>(null);
-    const [barberias,setBarberias]=useState<BarberiaBusqueda[]>([]);
-    const [isLoading,setIsLoading]=useState<boolean>(true);
+    const searchRef = useRef<HTMLDivElement>(null)
+    const [barberias,setBarberias]=useState<BarberiaBusqueda[]>([])
+    const [isLoading,setIsLoading]=useState<boolean>(true)
 
-    const [results,setResults]=useState<BarberiaBusqueda[]>([]);
+    const [results,setResults]=useState<BarberiaBusqueda[]>([])
     const callgetCompany=async()=>{
         try{
-            const response=await api.get("/api/company")
-            setBarberias(response.data);
-            setIsLoading(false);
+            const response=await api.get('/api/company')
+            setBarberias(response.data)
+            setIsLoading(false)
         }catch(error){
-            setBarberias([]);
+            setBarberias([])
             throw new Error ('ERROR: '+error)
            
         }
@@ -30,32 +30,32 @@ export const SearchSection = () => {
     
     const getBarberShopBySearchTerm=(term:string)=>{
         if(term===''){
-            setResults([]);
+            setResults([])
         }else{
-            const rpta=barberias.filter((barberia)=>barberia.name.toLowerCase().includes(term.toLowerCase()));
-            setResults(rpta);    
+            const rpta=barberias.filter((barberia)=>barberia.name.toLowerCase().includes(term.toLowerCase()))
+            setResults(rpta)    
         }
     }
     useEffect(()=>{
-        callgetCompany();
-    },[]);
+        callgetCompany()
+    },[])
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-          if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
-            setResults([]);
-          }
+            if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
+                setResults([])
+            }
         }
     
-        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('mousedown', handleClickOutside)
         return () => {
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-      }, []);
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
 
 
 
-    console.log(barberias);
+    console.log(barberias)
     console.log(results)
 
 
@@ -64,33 +64,33 @@ export const SearchSection = () => {
               
             <form className="flex w-full relative border rounded-xl border-cyan-300 " >
                 <input 
-                onChange={(e)=>{
-                    getBarberShopBySearchTerm(e.target.value)
-                }}
-                type="search" placeholder="Buscar barbería..."
+                    onChange={(e)=>{
+                        getBarberShopBySearchTerm(e.target.value)
+                    }}
+                    type="search" placeholder="Buscar barbería..."
                     className=" px-5 rounded-l-xl bg-[#3A3644] w-full text-white focus:outline-none"
                 />
                 <button className="bg-[#3A3644] p-3 rounded-r-xl "><FaSearch size={20} className="text-white" /></button>
                 <ul className={`absolute top-10 z-50 w-full rounded-b-xl bg-cyan-300 text-black max-h-32
                             overflow-y-scroll ${results?.length===0 && 'hidden'}`}>
-                {
-                    isLoading ? (
+                    {
+                        isLoading ? (
                             <div>Loading BarberShops...</div>
-                    ) :(
-                        results?.map((barberia)=>{
-                            return(
-                                <a className='w-full' href={`./new-reservation?barbershop=${barberia.id}`}>
-                                    <li className='w-full py-2 pl-4'>
-                                        {barberia.name}
-                                    </li>
-                                </a>
-                            )
-                        })
-                    )
+                        ) :(
+                            results?.map((barberia)=>{
+                                return(
+                                    <a className='w-full' href={`./new-reservation?barbershop=${barberia.id}`}>
+                                        <li className='w-full py-2 pl-4'>
+                                            {barberia.name}
+                                        </li>
+                                    </a>
+                                )
+                            })
+                        )
 
                     
-                }
-            </ul> 
+                    }
+                </ul> 
             
             
             </form>
